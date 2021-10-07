05108fba29ca09ef8b349ce59d572a85afe7a47e
GeonNamGung <rndrjs123@naver.com>
2021-10-07 19:56:24 +0900

    var-> const

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-var express   = require('express');
-var app       = express();
-var fs        = require('fs'); 
-var serverStatic = require('serve-static');
-var path = require('path');
-var cors = require('cors');
-var bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const serverStatic = require("serve-static");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-app.set('view engine', 'ejs');
-app.use(cors())
-app.use(bodyParser.json())
-app.use(serverStatic(path.join(__dirname, 'uploadedFiles')));
-app.use(serverStatic(path.join(__dirname, 'routes')));
-app.use('/', require('./routes/main'));
+app.set("view engine", "ejs");
+app.use(cors());
+app.use(bodyParser.json());
+app.use(serverStatic(path.join(__dirname, "uploadedFiles")));
+app.use(serverStatic(path.join(__dirname, "routes")));
+app.use("/", require("./routes/main"));
 
-var port = 3005;
-app.listen(port, function(){
-  var dir = './uploadedFiles';
-  var dir2 = './tempFile';
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir); 
-  if (!fs.existsSync(dir2)) fs.mkdirSync(dir2); 
-  
-  console.log('server on!');
+const PORT = 3005;
+app.listen(PORT, function () {
+  const dir = "./uploadedFiles";
+  const dir2 = "./tempFile";
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+  if (!fs.existsSync(dir2)) fs.mkdirSync(dir2);
+
+  console.log("server on!");
 });
 
-process.on('uncaughtException', console.error);
+process.on("uncaughtException", console.error);
